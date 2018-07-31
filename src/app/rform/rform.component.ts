048b6d42cbdd4d4c3603bcd23f29f1b70a57af9e
himanshuf25 <himanshuf22@hotmail.com>
2018-07-31 09:36:22 +0530

    another changes

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -16,8 +16,10 @@ export class RformComponent implements OnInit {
       contact:new FormControl('',[Validators.required,Validators.pattern("^(([0-9]*)|(([0-9]*)\.([0-9]*)))$")]),
       Gender:new FormControl('',[Validators.required,Validators.pattern("^[a-zA-Z]+$")]),
       EmpId:new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(4),Validators.pattern("^(([0-9]*)|(([0-9]*)\.([0-9]*)))$")]),
+      Email:new FormControl('',[Validators.required,Validators.email]),
       password:new FormControl('',[Validators.required]),
-      checkpassword:new FormControl('',[Validators.required])
+      checkpassword:new FormControl('',[Validators.required]),
+      
 
     }
   );
